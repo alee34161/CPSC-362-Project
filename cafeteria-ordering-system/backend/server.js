2ac0d91e71f39fcf16128f2c675684b0b249b917
kermikyu <109919916+kermikyu@users.noreply.github.com>
2025-04-05 14:23:01 -0700

    4/5 Testing

--- cafeteria-ordering-system/backend/server.js
+++ cafeteria-ordering-system/backend/server.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 
+
 var conn = mysql.createConnection({
 	host: "localhost",
 	user: "root",
--- cafeteria-ordering-system/frontend/.next/types/app/layout.ts
+++ cafeteria-ordering-system/frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /mnt/c/Users/crazy/Desktop/GitHub/CPSC-362-Project/cafeteria-ordering-system/frontend/src/app/layout.tsx
+// File: /home/andrew-lee/Documents/GitHub/CPSC-362-Project/cafeteria-ordering-system/frontend/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
