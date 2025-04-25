a0e35304611996f7d7294f9b96d69decf7f5c74a
Andrew Lee <alee34161@gmail.com>
2025-04-25 14:25:58 -0700

    Implemented Orders History

--- cafeteria-ordering-system/frontend/src/app/meal-subscription/page.tsx
+++ cafeteria-ordering-system/frontend/src/app/meal-subscription/page.tsx
@@ -4,7 +4,7 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
 export default function MealSubscriptionPage() {
-	const [subscribed, setSubscribed] = useState(null);
+	const [subscribed, setSubscribed] = useState<boolean | null>(null);
 
 	useEffect(() => {
 		axios.get('http://localhost:8080/currentuserread', { withCredentials: true })
@@ -56,6 +56,14 @@ export default function MealSubscriptionPage() {
         onClick={() => handleSubscribe()}>
           <button className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
             Change Subscription Status
+          </button>
+        </div>
+        <div className="mt-8 flex justify-center">
+          <button 
+            onClick={() => window.history.back()} 
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          >
+            Go Back
           </button>
         </div>
       </div>
